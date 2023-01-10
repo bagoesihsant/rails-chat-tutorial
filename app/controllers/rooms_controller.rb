4812e100279467d7c6f39a537d712dd61811653c
bagoesihsant <bagoesihsant@gmail.com>
2023-01-10 11:02:39 +0700

    New Commit, Finished Room UI

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,18 @@
 class RoomsController < ApplicationController
 
+    before_action :load_entities
+
     def index
         @rooms = Room.all
     end
 
     def new
         @room = Room.new
+    end
+
+    def show
+        @room_message = RoomMessage.new room: @room
+        @room_messages = @room.room_messages.includes(:user)
     end
 
     def create
